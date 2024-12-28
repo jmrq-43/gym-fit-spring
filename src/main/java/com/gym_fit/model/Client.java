9d5d27163e735ac821fec2dd70bbe8a64b6f5065
juan miguel <rojasquinteroj26@gmail.com>
2024-12-27 19:46:05 -0500

    added basic configuration for Client class

---
+++ src/main/java/com/gym_fit/model/Client.java
@@ -0,0 +1,15 @@
+package com.gym_fit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer membership;
+}
