7ba14793621b84de5ca9b1b300e4a17c18f0fdb9
juan miguel <rojasquinteroj26@gmail.com>
2024-12-27 20:45:10 -0500

    add listClients method

--- src/main/java/com/gym_fit/service/ClientService.java
+++ src/main/java/com/gym_fit/service/ClientService.java
@@ -1,15 +1,22 @@
 package com.gym_fit.service;
 
 import com.gym_fit.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.gym_fit.repository.ClientRepository;
 
 import java.util.List;
 
 @Service
 public class ClientService implements IClientService {
+
+    @Autowired
+    private ClientRepository clienteRepository;
+
     @Override
     public List<Client> listClients() {
-        return List.of();
+        List<Client> clients = clienteRepository.findAll();
+        return clients;
     }
 
     @Override
