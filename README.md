06cd66e179db834cf1bd7fcea9dbbb635525e2a8
juan miguel <rojasquinteroj26@gmail.com>
2024-12-27 19:24:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# GYM FIT (now using spring)
+
+---
+
---
+++ src/test/java/com/gym_fit/GymFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
