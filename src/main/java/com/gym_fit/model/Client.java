c18b5e7f6b8f6834a67dc37794a847780038bd8a
juan miguel <rojasquinteroj26@gmail.com>
2024-12-27 19:46:48 -0500

    added loombok notation for Client class

--- src/main/java/com/gym_fit/model/Client.java
+++ src/main/java/com/gym_fit/model/Client.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 @Entity
 public class Client {
     @Id
