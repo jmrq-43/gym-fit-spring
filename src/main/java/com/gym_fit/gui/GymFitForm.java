0216d0de769e68f47ccc28af65bdb4ad7740554b
juan miguel <rojasquinteroj26@gmail.com>
2025-01-02 15:18:06 -0500

    configurated list clients

--- src/main/java/com/gym_fit/gui/GymFitForm.java
+++ src/main/java/com/gym_fit/gui/GymFitForm.java
@@ -35,5 +35,20 @@ public class GymFitForm extends JFrame {
         String[] headers = {"id","name","last name","membership"};
         this.tableModel.setColumnIdentifiers(headers);
         this.clientTable = new JTable(tableModel);
+        listClients();
+    }
+
+    private void listClients() {
+        this.tableModel.setRowCount(0);
+        var client = this.clientService.listClients();
+        client.forEach(client1 -> {
+            Object[] clientLine = {
+                    client1.getId(),
+                    client1.getName(),
+                    client1.getLASTNAME(),
+                    client1.getMembership()
+            };
+            this.tableModel.addRow(clientLine);
+        });
     }
 }
